64d1369a8880931a4b7ebc6114c6d16766033ffd
jowardhar <mohammed.jowardhar@gmail.com>
2018-03-01 21:46:37 +0000

    Exercise 7 Completed!

---
+++ ex7.rb
@@ -0,0 +1,32 @@
+# prints string
+puts "Mary had a little lamb."
+# prints string with interpolation with string
+puts "It's fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+# prints string "." and multiply by 10
+puts "." * 10 # what'd that do
+
+# variables for each character
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+
+# prints variables beside one another
+print end1 + end2 + end3 + end4 + end5 + end6
+# prints variables beside the print output
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+# Study Drill 1 - Go back through and write a comment on what each line does.
+
+# Answer: Done
