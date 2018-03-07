cf8e5eb8b221491132e3b367f567ebe87895391d
jowardhar <mohammed.jowardhar@gmail.com>
2018-03-07 15:11:47 +0000

    ex19sd3.rb updated study drill.

--- ex19sd3.rb
+++ ex19sd3.rb
@@ -20,4 +20,15 @@ super_hero(6, 5, 7, 7 + power_boost)
 puts "Third Hero:"
 super_hero(6 + power_boost, 9 - power_loss, 7, 4)
 
+puts "Create your own Super Hero"
+print "Type your power: "
+power  = $stdin.gets.chomp.to_i
+print "Type your speed: "
+speed  = $stdin.gets.chomp.to_i
+print "Type your strength: "
+strength  = $stdin.gets.chomp.to_i
+print "Type your intellect: "
+intellect  = $stdin.gets.chomp.to_i
+
+super_hero(power * power_boost, speed / power_loss, strength + power_boost, intellect)
 
